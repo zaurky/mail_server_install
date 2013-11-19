f8f3c36172cb9feaf75543af8e35c2f3cffb4662
zaurky <zaurky@zeb.re>
2013-11-19 19:10:12 +0100

    add create_vm_auto technique

--- install_part1.sh
+++ install_part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function create_vm() {
+function create_vm_manual() {
     DOMAIN=$1
     echo "On the gandi web site :
      * create a new vm (wheezy - debian 7)
@@ -15,7 +15,42 @@ function create_vm() {
     read -p "Enter the ipv6 : " SERVER_IPV6
 }
 
+function create_vm_auto() {
+    DOMAIN=$1
+    echo "Please insert your gandi apikey"
+    read APIKEY
+
+    echo "Generating an ssh key put a password if you want"
+    ssh-keygen -f ~/.ssh/id_dsa.mail.$DOMAIN -t dsa
+
+    echo "Creating vm"
+    ./create_vm.py -s ~/.ssh/id_dsa.mail.$DOMAIN.pub \
+                   -d $DOMAIN -a $APIKEY | tee /tmp/create_vm.mail.$DOMAIN
+
+    SERVER_IP=`grep IPV4 /tmp/create_vm.mail.$DOMAIN | awk '{print $2}'`
+    SERVER_IPV6=`grep IPV6 /tmp/create_vm.mail.$DOMAIN | awk '{print $2}'`
+}
+
+function create_vm() {
+    DOMAIN=$1
+
+    echo "Do you want the vm to be automatically created at gandi ? (y/N)"
+    read -n 1 ANSWER
+
+    if [ "x$ANSWER" == "xY" ] || [ "x$ANSWER" == "xy" ]; then
+        create_vm_auto $DOMAIN
+    elif [ "x$ANSWER" == "xN" ] || [ "x$ANSWER" == "xn" ]; then
+        create_vm_manual $DOMAIN
+    else
+        create_vm_manual $DOMAIN
+    fi
+}
+
 function alter_domain_zone() {
+    DOMAIN=$1
+    SERVER_IP=$2
+    SERVER_IPV6=$3
+
     echo "Modify the domain zone to put :
 
 \"\"\"
@@ -51,10 +86,10 @@ create_vm $DOMAIN
 alter_domain_zone $DOMAIN $SERVER_IP $SERVER_IPV6
 
 echo "Copying the files we need on the server"
-scp -i ~/.ssh/id_dsa.$DOMAIN -r install_mail root@$SERVER_IP:/tmp/
+scp -i ~/.ssh/id_dsa.mail.$DOMAIN -r install_mail root@$SERVER_IP:/tmp/
 
 echo "Login to the server to execute the remaining install script"
-ssh -i ~/.ssh/id_dsa.$DOMAIN root@$SERVER_IP "cd /tmp/install_mail/; ./install_part2.sh $DOMAIN"
+ssh -i ~/.ssh/id_dsa.mail.$DOMAIN root@$SERVER_IP "cd /tmp/install_mail/; ./install_part2.sh $DOMAIN"
 
 echo "Cleaning files on server"
 ssh root@$SERVER_IP "rm -fr /tmp/install_mail/"
